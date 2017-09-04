c85c7c98b926cdcf96612b224d3e7ecfea727e06
sethcarrino <sethcarrino@gmail.com>
2017-09-04 11:59:35 -0400

    Added redux folders and files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
+//react imports
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+import './styles/index.css';
+
+
+
+//redux imports
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
+//import promise from 'redux-promise';
+
+//react router imports
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+
+//components
+import App from './components/App';
+
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+//switch uses most specific route that matches, top down.
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
